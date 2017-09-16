b37e58229ff286907aa2e659cce0e33096989c26
Andrew Walz <andrewjwalz@gmail.com>
2017-09-16 15:08:47 -0600

    Updated pods and removed activity view

--- ArkMonitor/Misc/ArkActivityView.swift
+++ ArkMonitor/Misc/ArkActivityView.swift
@@ -7,19 +7,9 @@
 //
 
 import UIKit
-import NVActivityIndicatorView
 import NotificationBannerSwift
 
 public struct ArkActivityView  {
-    static private let activityData = ActivityData(type: NVActivityIndicatorType.lineScale)
-
-    static public func startAnimating() {
-        NVActivityIndicatorPresenter.sharedInstance.startAnimating(activityData)
-    }
-    
-    static public func stopAnimating() {
-        NVActivityIndicatorPresenter.sharedInstance.stopAnimating()
-    }
     
     static public func showMessage(_ text: String) {
         let banner = StatusBarNotificationBanner(title: text, style: .warning, colors: CustomBannerColors())
--- Podfile
+++ Podfile
@@ -2,14 +2,8 @@
 # platform :ios, '9.0'
 
 target 'ArkMonitor' do
-  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
-
-  # Pods for ArkMonitor
-
-  pod 'Alamofire', '~> 4.3'
-  pod 'NotificationBannerSwift'
-  pod 'NVActivityIndicatorView', :git => 'https://github.com/ninjaprox/NVActivityIndicatorView.git', :branch => 'swift4'
-  pod 'SnapKit', :git => 'https://github.com/SnapKit/SnapKit.git', :branch => 'swift-4'
-
+    pod 'Alamofire', '~> 4.3'
+    pod 'NotificationBannerSwift'
+    pod 'SnapKit', :git => 'https://github.com/SnapKit/SnapKit.git', :branch => 'swift-4'
 end
